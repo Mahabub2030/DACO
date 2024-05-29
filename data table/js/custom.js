0dbc000bf6a903e221eba73784c17852dab71dde
mahabub <mahabubalom72091@gmail.com>
2024-05-29 13:04:49 +0300

    addng

--- data table/js/custom.js
+++ data table/js/custom.js
@@ -192,6 +192,81 @@
 
 
 // // randome code
+// $(document).ready(function() {
+//     // Get the current date
+//     var currentDate = new Date();
+//     // Get the month and convert it to a string with the full month name
+//     var currentMonth = currentDate.toLocaleString('en-US', { month: 'long' });
+
+//     var table = $('#example').DataTable({
+//         buttons: [
+//             'copy', 
+//             'csv', 
+//             'excel', 
+//             {
+//                 extend: 'pdf',
+//                 customize: function (doc) {
+//                     // Change page size and orientation to A3
+//                     doc.pageOrientation = 'landscape';
+//                     doc.pageSize = 'A3';
+//                     doc.defaultStyle.fontSize = 12;
+                    
+//                     // Apply text-center class to 9th and 10th columns in PDF
+//                     doc.content[1].table.body.forEach(function(row) {
+//                         row[8].alignment = 'center'; // 9th column alignment
+//                         row[9].alignment = 'center'; // 10th column alignment
+//                     });
+
+//                     // Increase font size for header
+//                     doc.content[1].table.body.forEach(function
+//                         (row){
+//                             row.forEach(function(cell){
+//                                 cell.fontSize =8;
+//                             })
+//                         }
+//                     )
+//                     // doc.content[1].table.headerRows = 1; // Make sure header is only shown once
+//                     // doc.styles.tableHeader.fontSize = 10; // Set font size for header
+//                     // doc.styles.tableHeader.alignment = 'center'
+//                 }
+//             }, 
+//             'print', 
+//             'add', 
+//             'remove'
+//         ]
+//     });
+
+//     // Update the title to display the current month
+//     $('.title').text("Manpower List Month Of " + currentMonth + "-" + currentDate.getFullYear());
+
+//     // Apply text-center class to all columns except the first three and last one
+//     table.columns().every(function() {
+//         var colIdx = this.index();
+//         if (colIdx !== 0 && colIdx !== 1 && colIdx !== 2 && colIdx !== table.columns().count() - 1) {
+//             table.column(colIdx).nodes().to$().addClass('text-center');
+//         }
+//     });
+
+//     // Calculate total present and total holidays for each row
+//     table.rows().every(function() {
+//         var rowData = this.data();
+//         var totalPresent = 0;
+//         var totalHoliday = 0;
+//         for (var i = 4; i < rowData.length - 1; i++) { // Exclude first three columns and the last one
+//             if (rowData[i] === 'P') {
+//                 totalPresent++;
+//             } else if (rowData[i] === 'H') {
+//                 totalHoliday++;
+//             }
+//         }
+//         rowData[rowData.length - 1] = 'P: ' + totalPresent + ', H: ' + totalHoliday;
+//         this.data(rowData);
+//     });
+
+//     table.buttons().container().appendTo('#example_wrapper .col-md-6:eq(0)');
+// });
+
+
 $(document).ready(function() {
     // Get the current date
     var currentDate = new Date();
@@ -206,10 +281,10 @@ $(document).ready(function() {
             {
                 extend: 'pdf',
                 customize: function (doc) {
-                    // Change page size and orientation to A3
+                    // Change page size and orientation to A4
                     doc.pageOrientation = 'landscape';
                     doc.pageSize = 'A3';
-                    doc.defaultStyle.fontSize = 12;
+                    doc.defaultStyle.fontSize = 10;
                     
                     // Apply text-center class to 9th and 10th columns in PDF
                     doc.content[1].table.body.forEach(function(row) {
@@ -217,17 +292,12 @@ $(document).ready(function() {
                         row[9].alignment = 'center'; // 10th column alignment
                     });
 
-                    // Increase font size for header
-                    doc.content[1].table.body.forEach(function
-                        (row){
-                            row.forEach(function(cell){
-                                cell.fontSize =8;
-                            })
-                        }
-                    )
-                    // doc.content[1].table.headerRows = 1; // Make sure header is only shown once
-                    // doc.styles.tableHeader.fontSize = 10; // Set font size for header
-                    // doc.styles.tableHeader.alignment = 'center'
+                    // Increase font size for all cells
+                    doc.content[1].table.body.forEach(function(row) {
+                        row.forEach(function(cell) {
+                            cell.fontSize = 8;
+                        });
+                    });
                 }
             }, 
             'print', 
@@ -265,4 +335,5 @@ $(document).ready(function() {
 
     table.buttons().container().appendTo('#example_wrapper .col-md-6:eq(0)');
 });
+
 
